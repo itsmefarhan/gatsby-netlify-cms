44f417756a382ac52835f4c214e655e1c8498122
Farhan Farooq <farhan.farooq@live.com>
2021-02-27 00:27:06 +0500

    rendered dynamic pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import styles from './Layout.module.css';
+import React from "react"
+import styles from "./Layout.module.css"
+import { Link } from "gatsby"
 
 export default function Layout({ children }) {
   return (
     <div>
       <header id={styles.header}>
         <div id={styles.inner}>
-          <h1>Finest Coffee Shop</h1>
+          <h1>
+            <Link to="/">Finest Coffee Shop</Link>
+          </h1>
         </div>
       </header>
-      <main id={styles.main}>
-        {children}
-      </main>
+      <main id={styles.main}>{children}</main>
     </div>
-  );
+  )
 }
