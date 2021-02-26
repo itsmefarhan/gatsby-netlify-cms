dc0b5d013763a88af0cb61d7a31fe4069c30a3ea
Farhan Farooq <farhan.farooq@live.com>
2021-02-27 00:09:36 +0500

    blog list component

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "The Coffee Blog",
   },
-  plugins: ["gatsby-plugin-netlify-cms"],
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    {
+      resolve: "gatsby-source-filesystem", // to load files
+      options: {
+        name: "blog",
+        path: "src/blog",
+      },
+    },
+    // to parse the Markdown data and front matter and add this information to the GraphQL schema
+    "gatsby-transformer-remark",
+  ],
 }
